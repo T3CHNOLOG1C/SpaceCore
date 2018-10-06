5913007e383dcf7ce0c887d69f6070ccebf8562e
Jan200101 <sentrycraft123@gmail.com>
2018-10-06 18:21:18 +0200

    [Utils] add checks

---
+++ addons/utils/checks.py
@@ -0,0 +1,13 @@
+from discord.ext.commands import check
+
+
+def is_owner():
+    return check((lambda ctx: ctx.bot.owner_role in ctx.message.author.roles))
+
+
+def is_admin():
+    return check((lambda ctx: ctx.bot.admin_role in ctx.message.author.roles))
+
+
+def is_staff():
+    return is_owner() or is_admin()
