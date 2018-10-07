418fa89c2255a6ae2441554c213d3e9b3386b976
Jan200101 <sentrycraft123@gmail.com>
2018-10-07 14:10:32 +0200

    [Logger] various changes - add color to embeds - remove format as a argument - add discord message fallback - move time related functions to the log writing function

--- addons/utils/logger.py
+++ addons/utils/logger.py
@@ -1,52 +1,48 @@
 from datetime import date, datetime
 from sys import _getframe
 from asyncio import ensure_future
-from discord import Embed
+from discord import Embed, Color
+from discord.errors import Forbidden
 from addons.utils.formatting import escape
+
 
 class Logger:
 
-    def __init__(self, name, channel=None, *, stftime="%d/%m/%Y %H:%M", writeformat="[{time}] {type} {name} {function}: {content}\n"):
+    def __init__(self, name, channel=None, *, stftime="%d/%m/%Y %H:%M"):
         self.name = name
         self.channel = channel
         self.strftime = stftime
-        self.writeformat = writeformat
 
     def write(self, content, type, function):
         time = datetime.now().strftime(self.strftime)
         with open("logs/{}.log".format(str(date.today())), "a") as file:
-            file.write(self.writeformat.format(time=time, type=type,
-                                               name=self.name, function=function, content=content))
+            file.write("[{time}] {type} {name} {function}: {content}\n".format(
+                time=time, type=type, name=self.name, function=function, content=content))
 
-    def send(self, content, type, function):
+    def send(self, content, type, function, color="#ffffff"):
         async def wrapper(self, content, type, function):
-            emb = Embed()
-            emb.add_field(name="Name", value=escape(self.name))
+            emb = Embed(color=color)
             emb.add_field(name="Type", value=type)
+            emb.add_field(name="Name", value=escape(self.name))
             emb.add_field(name="Function", value=escape(function))
-            emb.add_field(name="Message", value=escape(content))
-            await self.channel.send(embed=emb)
+            emb.add_field(name="Message", value=escape(content), inline=True)
+            try:
+                await self.channel.send(embed=emb)
+            except:
+                await self.channel.send("{type} {name} {function}: {content}\n".format(type=type, name=self.name, function=function, content=content))
         ensure_future(wrapper(self, content, type, function))
 
     def info(self, content):
         type = "INFO"
         function = _getframe(1).f_code.co_name
-        time = datetime.now().strftime(self.strftime)
-        content = self.writeformat.format(
-            time=time, type=type, name=self.name, function=function, content=content)
-
         self.write(content, type, function)
         if self.channel:
-            self.send(content, type, function)
+            self.send(content, type, function, Color.green())
 
     def warn(self, content):
         type = "WARNING"
         function = _getframe(1).f_code.co_name
-        time = datetime.now().strftime(self.strftime)
-        content = self.writeformat.format(
-            time=time, type=type, name=self.name, function=function, content=content)
-
         print(content)
         self.write(content, type, function)
         if self.channel:
-            self.send(content, type, function)
+            self.send(content, type, function, Color.red())
