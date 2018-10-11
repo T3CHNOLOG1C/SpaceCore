a005f9904a3228b7f48f51e24d69df18e61dbba4
Jan200101 <sentrycraft123@gmail.com>
2018-10-11 17:40:52 +0200

    [Moderation] add ban and kick commands

--- addons/moderation.py
+++ addons/moderation.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3.6
-import datetime
-import discord
+from discord import Member
+from discord.errors import Forbidden
 from discord.ext import commands
-import botconfig
+
 
 class Moderation:
     """
@@ -11,9 +10,69 @@ class Moderation:
 
     def __init__(self, bot):
         self.bot = bot
-        print("{} addon loaded.".format(self.__class__.__name__))
-    
-        
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command()
+    async def kick(self, ctx, member: Member, *, reason: str=None):
+        """Kick a member. (Staff Only)"""
+        if member == ctx.message.author:
+            await ctx.send("You cannot kick yourself")
+            return
+
+        elif member == ctx.me:
+            await ctx.send("Unable to kick myself")
+            return
+
+        elif self.bot.owner_role in member.roles:
+            await ctx.send("Unable to kick Owner")
+
+        elif self.bot.admin_role in member.roles and not self.bot.owner_role in ctx.message.author.roles:
+            await ctx.send("Unable to kick Admin")
+
+        msg = "You have been kicked from {}".format(ctx.guild.name)
+        if reason:
+            msg += " for the following reason:\n{}".format(reason)
+
+        try:
+            await member.send(msg)
+        except Forbidden:
+            await ctx.send("User had DMs disabled")
+
+        await member.kick()
+
+        await ctx.send("{} has been kicked".format())
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def ban(self, ctx, member: Member, *, reason: str=None):
+        """Ban a member. (Staff Only)"""
+        if member == ctx.message.author:
+            await ctx.send("You cannot ban yourself")
+            return
+
+        elif member == ctx.me:
+            await ctx.send("Unable to ban myself")
+            return
+
+        elif self.bot.owner_role in member.roles:
+            await ctx.send("Unable to ban Owner")
+
+        elif self.bot.admin_role in member.roles and not self.bot.owner_role in ctx.message.author.roles:
+            await ctx.send("Unable to ban Admin")
+
+        msg = "You have been banned from {}".format(ctx.guild.name)
+        if reason:
+            msg += " for the following reason:\n{}".format(reason)
+
+        try:
+            await member.send(msg)
+        except Forbidden:
+            await ctx.send("User had DMs disabled")
+
+        await member.ban(delete_message_days=0)
+
+        await ctx.send("{} has been banned".format())
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
